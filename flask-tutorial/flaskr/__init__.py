31ff0529e779cf2446749de2523def3af3a8ee4c
Renanlneves <renanlneves@gmail.com>
2023-06-19 16:33:01 -0300

    criando funcao modulo app

---
+++ flask-tutorial/flaskr/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+
+def create_app(test_config= None):
+    #cria e configura o app
+    app = Flask(__name__, instance_relative_config= True)
+    app.config.from_mapping(
+        SECRET_KEY= 'dev',
+        DATABASE= os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        # carrega a configuração da instância, se existir, quando não estiver testando
+        app.config.from_pyfile('config.py', silent= True)
+    else:
+        #se for aprovado, carrega a configuração de teste
+        app.config.from_mapping(test_config)
+
+    #se sertifica que a pasta da instancia exista
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #uma pagina simples que diz hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello world'
+    
+    return app
