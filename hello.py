cc0b3c626f28b48387bda058f81619560e4cee9f
Renanlneves <renanlneves@gmail.com>
2023-06-19 11:32:40 -0300

    fim do quickstart

--- hello.py
+++ hello.py
@@ -89,19 +89,19 @@ with app.test_request_context():
 É importante se familiarizar com os métodos HTTP ao trabalhar com Flask. 
 Por padrão, uma rota responde apenas a solicitações GET. 
 Você pode usar o argumento methods do decorador route() para lidar com diferentes métodos HTTP."""
-
+"""
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         return do_the_login()
     else:
         return show_the_login_form()
-    
+    """
 """O exemplo acima mantém todos os métodos da rota dentro de uma única função, o que pode ser útil se cada parte utilizar alguns dados comuns.
 Você também pode separar as visualizações para diferentes métodos em funções diferentes. 
 O Flask fornece um atalho para decorar essas rotas com get(), post(), etc., para cada método HTTP comum.
 """
-
+"""
 app.get("/login")
 def login_get():
     return show_the_login_form()
@@ -109,3 +109,22 @@ def login_get():
 app.post("/login")
 def login_post():
     return do_the_login()
+
+"""
+
+#The Request Object
+"""
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    error = None
+    if request.method == "POST":
+        if valid_login(request.form["username"],
+                       request.form["password"]):
+            return log_the_user_in(request.form["username"])
+        else:
+            error = "Invalid username/password"
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+    """
+
