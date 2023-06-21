93610b69c9b9e2151fa65c3f0cdaedcce75c9d8a
Renanlneves <renanlneves@gmail.com>
2023-06-21 15:13:51 -0300

    correcao de bugs

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -21,7 +21,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSTERT INTO user (username, password) VALUES (?, ?)",
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
